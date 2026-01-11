43303882235b7a2c9bef6e9ab5f215395da0b509
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-11 11:53:44 -0300

    chore: initialize project with basic configuration and structure

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import tseslint from "typescript-eslint"
+import reactX from "eslint-plugin-react-x"
+import reactDom from "eslint-plugin-react-dom"
+import eslintConfigPrettier from "eslint-config-prettier"
+import { defineConfig, globalIgnores } from "eslint/config"
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      reactX.configs["recommended-typescript"],
+      reactDom.configs.recommended,
+      eslintConfigPrettier,
+    ],
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: globals.browser,
+    },
+    rules: {
+      semi: ["warn", "never"],
+      eqeqeq: ["error", "always"],
+    },
+  },
+])
