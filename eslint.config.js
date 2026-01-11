36ee768f81061b98d4ebcc3477fd2561ccfc4157
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-11 12:53:19 -0300

    style: format code with prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,33 @@
-import js from "@eslint/js"
-import globals from "globals"
-import reactHooks from "eslint-plugin-react-hooks"
-import reactRefresh from "eslint-plugin-react-refresh"
-import tseslint from "typescript-eslint"
-import reactX from "eslint-plugin-react-x"
-import reactDom from "eslint-plugin-react-dom"
-import eslintConfigPrettier from "eslint-config-prettier"
-import { defineConfig, globalIgnores } from "eslint/config"
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(['dist']),
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-      reactX.configs["recommended-typescript"],
+      reactX.configs['recommended-typescript'],
       reactDom.configs.recommended,
       eslintConfigPrettier,
     ],
     languageOptions: {
-      ecmaVersion: "latest",
+      ecmaVersion: 'latest',
       globals: globals.browser,
     },
     rules: {
-      semi: ["warn", "never"],
-      eqeqeq: ["error", "always"],
+      semi: ['warn', 'never'],
+      eqeqeq: ['error', 'always'],
     },
   },
 ])
