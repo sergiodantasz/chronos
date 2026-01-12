ad75dc4cb3e539f3d74aee367c4d0c778cb5ca3b
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-12 11:33:07 -0300

    feat: add CountDown component and integrate it into App structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Container } from './components/Container'
+import { CountDown } from './components/CountDown'
 import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
 
@@ -10,6 +11,9 @@ export const App = () => {
       </Container>
       <Container>
         <Menu />
+      </Container>
+      <Container>
+        <CountDown />
       </Container>
     </>
   )
