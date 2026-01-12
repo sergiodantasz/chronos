a5c01b59e35ba78730dfec1b6422c12927470726
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-12 08:42:58 -0300

    refactor: format code with prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import reactDom from 'eslint-plugin-react-dom'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 import reactX from 'eslint-plugin-react-x'
-import reactDom from 'eslint-plugin-react-dom'
-import eslintConfigPrettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig([
   globalIgnores(['dist']),
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import { App } from './App.tsx'
+
+import styles from './App.module.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <div className={styles.app}>pi</div>
   </StrictMode>,
 )
