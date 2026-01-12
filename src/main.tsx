b1c3240a69edf01816cd1954c36377247d34c125
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-12 09:03:10 -0300

    style: add global and theme stylesheets for consistent design

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 
 import { App } from './App.tsx'
 
+import './styles/global.css'
+import './styles/theme.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
