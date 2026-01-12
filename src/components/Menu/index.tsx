565e55017d339abbab87b33fe0b56365410110ad
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-12 19:32:16 -0300

    feat: implement theme toggle functionality in Menu component and update styles for light theme

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,33 +1,65 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react'
+import { useEffect, useState } from 'react'
+
+import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon, SunIcon } from 'lucide-react'
 
 import styles from './styles.module.css'
 
+type Themes = 'dark' | 'light'
+
+const nextThemeIcons = {
+  dark: <MoonIcon />,
+  light: <SunIcon />,
+}
+
 export const Menu = () => {
+  const [theme, setTheme] = useState<Themes>(() => {
+    return (localStorage.getItem('theme') as Themes) || 'dark'
+  })
+
+  const handleTheme = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault()
+    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'))
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme)
+    localStorage.setItem('theme', theme)
+  }, [theme])
+
   return (
     <nav className={styles.menu}>
       <a
         className={styles.menuLink}
         href=''
+        aria-label='Home'
+        title='Home'
       >
         <HouseIcon />
       </a>
       <a
         className={styles.menuLink}
         href=''
+        aria-label='History'
+        title='History'
       >
         <HistoryIcon />
       </a>
       <a
         className={styles.menuLink}
         href=''
+        aria-label='Settings'
+        title='Settings'
       >
         <SettingsIcon />
       </a>
       <a
         className={styles.menuLink}
         href=''
+        aria-label='Toggle theme'
+        title='Toggle theme'
+        onClick={handleTheme}
       >
-        <SunIcon />
+        {nextThemeIcons[theme]}
       </a>
     </nav>
   )
