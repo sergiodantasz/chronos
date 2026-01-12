59f7eca4fa96b41f6e5c9373546eb0f7129c600d
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-12 08:49:23 -0300

    refactor: remove unused App.module.css and associated styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,8 @@ import { createRoot } from 'react-dom/client'
 
 import { App } from './App.tsx'
 
-import styles from './App.module.css'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <div className={styles.app}>pi</div>
   </StrictMode>,
 )
