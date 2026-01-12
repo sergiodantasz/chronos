7d1d4f1949f6b2e5cae0b10292bfcc07c364abd2
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-12 20:47:13 -0300

    feat: enhance NotFound page with a more engaging 404 message and structured layout using GenericHtml

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,17 +1,32 @@
 import { MainTemplate } from '../../templates/MainTemplate'
 
 import { Container } from '../../components/Container'
+import { GenericHtml } from '../../components/GenericHtml'
+import { Heading } from '../../components/Heading'
 
 export const NotFound = () => {
   return (
     <MainTemplate>
       <Container>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore illum eius suscipit voluptate accusantium
-          voluptas, similique autem modi eaque ab nesciunt corporis repudiandae dolor voluptatem velit commodi esse at
-          minus obcaecati sed aliquam aperiam itaque numquam. Impedit et dolor hic odio nisi, consectetur quod error
-          vero amet dolorem corrupti in!
-        </p>
+        <GenericHtml>
+          <Heading>404 - Page Not Found 🚀</Heading>
+          <p>
+            Oops! It looks like the page you're trying to access doesn't exist. Maybe it went on vacation, decided to
+            explore the universe, or got lost somewhere between two black holes. 🌌
+          </p>
+          <p>
+            But don't worry, you're not lost in space (yet). You can safely head back to the <a href='/'>home page</a>{' '}
+            or <a href='/history'>the history page</a> — or you can stay here and pretend you've found a secret page
+            that only the coolest explorers can access. 🧭✨
+          </p>
+          <p>
+            If you think this page should exist (or if you just want to chat about time travel and wormholes), feel free
+            to get in touch. Otherwise, use the menu to return to the real world.
+          </p>
+          <p>
+            In the meantime, here's a thought: “If a page doesn't exist on the internet, did it ever really exist?” 🤔💭
+          </p>
+        </GenericHtml>
       </Container>
     </MainTemplate>
   )
