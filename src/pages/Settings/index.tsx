ed7fd3a2b2c8eee6a74bb957546b950336f04c00
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-14 13:44:13 -0300

    feat: enhance Settings page with input handling for focus and break time configuration

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,3 +1,5 @@
+import { useRef } from 'react'
+
 import { MainTemplate } from '../../templates/MainTemplate'
 
 import { Container } from '../../components/Container'
@@ -5,11 +7,28 @@ import { DefaultButton } from '../../components/DefaultButton'
 import { DefaultInput } from '../../components/DefaultInput'
 import { Heading } from '../../components/Heading'
 
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext'
+
 import { SaveIcon } from 'lucide-react'
 
 import styles from './styles.module.css'
 
 export const Settings = () => {
+  const { state } = useTaskContext()
+  const focusTimeInput = useRef<HTMLInputElement>(null)
+  const shortBreakTimeInput = useRef<HTMLInputElement>(null)
+  const longBreakTimeInput = useRef<HTMLInputElement>(null)
+
+  const handleSaveSettings = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const focusTime = focusTimeInput.current?.value
+    const shortBreakTime = shortBreakTimeInput.current?.value
+    const longBreakTime = longBreakTimeInput.current?.value
+
+    console.log(focusTime, shortBreakTime, longBreakTime)
+  }
+
   return (
     <MainTemplate>
       <Container>
@@ -21,24 +40,34 @@ export const Settings = () => {
       <Container>
         <form
           action=''
+          onSubmit={handleSaveSettings}
           className={styles.form}
         >
           <div className={styles.formRow}>
             <DefaultInput
               id='focusTime'
               labelText='Focus:'
+              type='number'
+              ref={focusTimeInput}
+              defaultValue={state.config.focusTime}
             />
           </div>
           <div className={styles.formRow}>
             <DefaultInput
               id='shortBreakTime'
               labelText='Short break:'
+              type='number'
+              ref={shortBreakTimeInput}
+              defaultValue={state.config.shortBreakTime}
             />
           </div>
           <div className={styles.formRow}>
             <DefaultInput
               id='longBreakTime'
               labelText='Long break:'
+              type='number'
+              ref={longBreakTimeInput}
+              defaultValue={state.config.longBreakTime}
             />
           </div>
           <div className={styles.formRow}>
